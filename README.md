b6a33982e289857d8444ca79257d421796753dd2
athmanazampay <105713521+athmanazampay@users.noreply.github.com>
2022-11-01 12:14:59 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,105 @@
-# AzamPayWooCommercePlugin
-AzamPay WooCommerce Plugin
+=== WooCommerce AzamPay ===
+Contributors: azampay
+Tags: woocommerce, payment request, azampay, azampesa, mobile money, tigopesa, airtel money, halopesa, tanzania, online payments, mpesa, malipo
+Requires at least: 6.0
+Tested up to: 6.0
+Requires PHP: 7.0
+Stable tag: 1.0.0
+License: GPL-2.0+
+License URI: https://www.gnu.org/licenses/gpl-3.0.txt
+
+Acquire consumer payments from all electronic money wallets in Tanzania.
+
+== Description ==
+AzamPay Momo woocommerce is a wordpress plugin that allows merchants to receive mobile money payments through their woocommerce checkout pages. This allows merchants to receive payments from their AzamPesa, TigoPesa, AirtelMoney, and HaloPesa customers.
+This plugin works both sandbox and production environment.
+
+= Take AzamPay payments easily and directly on your store =
+
+The plugin extends WooCommerce allowing you to take payments directly on your store via AzamPay’s API.
+
+AzamPay is available for Store Owners and Merchants in Tanzania.
+
+= Use Of Azampay Woocommerce Plugin as Third-Party Service =
+
+This plugin is developed to enable merchants and businesses to receive payment through AzamPay payment gateway provided by AzamPay Tanzania Limited
+Upon checkout, a push request is sent to the customers handset where they can then confirm payment.
+
+== User Manual ==
+
+= Test Environment =
+
+1. Go to [Sandbox](https://developers.azampay.co.tz/) and register your website.
+1. From the sandbox retrieve your credentials.
+1. Go back to the plugin settings and enable Test Mode.
+1. Enter your credentials to the plugin settings.
+1. Test payments from the checkout Page.
+
+= Live Environment =
+
+1. Submit KYCs to AzamPay to get Live credentials.
+1. Disable Test Mode in the Plugin settings.
+1. Enter live credentials. Checkout is now enabled for live environment.
+
+## SUPPORTED CURRENCIES
+TZS
+
+## SUPPORTED MOBILE MONEY CHANNELS
+
+* AzamPesa
+* AirtelMoney
+* Halopesa
+* TigoPesa 
+
+
+== Installation ==
+This section describes how to install the plugin and get it working.
+
+= Automatic installation =
+
+Automatic installation is the easiest option as WordPress handles the file transfers itself and you don’t need to leave your web browser. To do an automatic install of the WooCommerce AzamPay plugin, log in to your WordPress dashboard, navigate to the Plugins menu and click Add New.
+
+In the search field type “WooCommerce AzamPay” and click Search Plugins. Once you’ve found our plugin you can view details about it such as the description. Most importantly, of course, you can install it by simply clicking "Install Now", then "Activate".
+
+= Manual installation =
+
+The manual installation method involves downloading our plugin and uploading it to your web server via your favorite FTP application. The WordPress codex contains [instructions on how to do this here](http://codex.wordpress.org/Managing_Plugins#Manual_Plugin_Installation).
+
+* Unzip woocommerce-azampay.zip to the /wp-content/plugins/ directory, or install from the WordPress Plugins Directory.
+* Activate the plugin through the ‘Plugins’ menu in WordPress.
+* Configure plugin under WooCommerce->Payments, look for AzamPay.
+
+== Frequently Asked Questions ==
+
+= How do I approve the Payment? =
+
+After submitting the payment through MOMO, confirm the payment on your phone by entering the PIN number.
+
+= Which number is the payment request sent to? =
+
+The payment request is sent to the mobile number provided during the checkout process.
+
+= What is the format of mobile number? =
+
+For Tanzania 255xxxxxxxxx or xxxxxxxxx or 0xxxxxxxxx
+NO ‘+’ sign before country code
+
+== Important Links ==
+1. https://authenticator.azampay.co.tz/ and https://authenticator-sandbox.azampay.co.tz/ are authentication URLs to get the access token for the Checkout API of AzamPay Payment Gateway in the sandbox and production environment respectively. 
+2. https://sandbox.azampay.co.tz/ and https://checkout.azampay.co.tz/ are used in the sandbox and production environment respectively to process checkout payment request and related APIs.
+
+== Screenshots ==
+
+1. The AzamPay Gateway settings screen used to configure the main azampay gateway.
+2. Checkout page that offers a range of payment methods such as local and alternative payment methods.
+
+== Changelog ==
+
+= 1.0.0 =
+* Our first version with WooCommerce AzamPay Live and Sandbox. 
+* AzamPay Checkout API is fully supported.
+
+== Upgrade Notice ==
+
+= 1.0 =
+First version of the plugin.
---
+++ woocommerce-azampay.php
@@ -0,0 +1,152 @@
+<?php
+
+/**
+ * Plugin Name: WooCommerce AzamPay
+ * Plugin URI: https://azampay.co.tz/
+ * Description: Acquire consumer payments from all electronic money wallets in Tanzania.
+ * Version: 1.0.0
+ * Author: AzamPay
+ * Author URI: https://azampay.co.tz/
+ * Requires PHP: 7.0
+ * WC requires at least: 6.0.0
+ * WC tested up to: 6.0.1
+ * Text Domain: azampay-woo
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+defined('ABSPATH') || exit;
+
+/**
+ * Initialize AzamPay WooCommerce payment gateway.
+ */
+function wc_azampay_payment_init() {
+
+    if (!class_exists('WC_Payment_Gateway')) {
+        add_action('admin_notices', 'wc_azampay_missing_notice');
+        return;
+    }
+
+    add_action('admin_notices', 'wc_azampay_testmode_notice');
+
+    require_once dirname(__FILE__) . '/includes/class-wc-gateway-azampay.php';
+
+    add_filter('woocommerce_payment_gateways', 'wc_add_azampay', 99);
+
+    add_filter('woocommerce_currencies', 'wc_add_currencies');
+
+    add_filter('woocommerce_currency_symbol', 'wc_add_currencies_symbol', 10, 2);
+
+    add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'woo_azampay_plugin_action_links');
+
+    add_filter('plugin_row_meta', 'woo_azampay_plugin_row_meta', 10, 2);
+}
+
+add_action('plugins_loaded', 'wc_azampay_payment_init', 99);
+
+/**
+ * Add Settings link to the plugin entry in the plugins menu.
+ *
+ * @param array $links Plugin action links.
+ *
+ * @return array
+ * */
+function woo_azampay_plugin_action_links($links) {
+    $settings_link = array('<a href="' .
+        admin_url('admin.php?page=wc-settings&tab=checkout&section=azampay') .
+        '" title="' . __('View AzamPay WooCommerce Settings', 'azampay-woo') . '">'
+        . __('Settings') . '</a>');
+
+    return array_merge($settings_link, $links);
+}
+
+/**
+ * Show row meta on the plugin screen.
+ *
+ * @param mixed $links Plugin Row Meta.
+ *
+ * @return array
+ */
+function woo_azampay_plugin_row_meta($links) {
+
+    /**
+     * The AzamPay Terms and Conditions URL.
+     */
+    $tnc_url = apply_filters('azampay_tnc_url', plugins_url('/includes/assets/docs/Terms_and_Conditions.pdf', __FILE__));
+
+    /**
+     * The AzamPay Privacy Policy URL.
+     */
+    $pp_url = apply_filters('azampay_pp_url', plugins_url('/includes/assets/docs/Privacy_Policy_V.1.0.pdf', __FILE__));
+
+    $row_meta = array(
+        'tnc' => '<a href="' . esc_url($tnc_url) . '" aria-label="' . esc_attr__('View AzamPay terms and conditions', 'azampay-woo') . '">' . esc_html__('Terms and Conditions', 'azampay-woo') . '</a>',
+        'pp' => '<a href="' . esc_url($pp_url) . '" aria-label="' . esc_attr__('View AzamPay privacy policy', 'azampay-woo') . '">' . esc_html__('Privacy Policy', 'azampay-woo') . '</a>',
+    );
+
+    return array_merge($links, $row_meta);
+}
+
+/**
+ * Add AzamPay Gateway to WooCommerce.
+ *
+ * @param array $methods WooCommerce payment gateways methods.
+ *
+ * @return array
+ */
+function wc_add_azampay($methods) {
+    $methods[] = 'WC_Gateway_Azampay'; // payment gateway class name
+    return $methods;
+}
+
+/**
+ * Add AzamPay Gateway to WooCommerce.
+ *
+ * @param array $currencies WooCommerce currencies.
+ *
+ * @return array
+ */
+function wc_add_currencies($currencies) {
+    $currencies['TZS'] = __('Tanzanian Shillings', 'azampay-woo');
+    return $currencies;
+}
+
+/**
+ * Add AzamPay Gateway to WooCommerce.
+ *
+ * @param string $currency_symbol WooCommerce currency symbol.
+ * @param string $currency WooCommerce currency.
+ *
+ * @return string
+ */
+function wc_add_currencies_symbol($currency_symbol, $currency) {
+    switch ($currency) {
+        case 'TZS':
+            $currency_symbol = 'TZS';
+            break;
+    }
+    return $currency_symbol;
+}
+
+/**
+ * Display a notice if WooCommerce is not installed
+ */
+function wc_azampay_missing_notice() {
+    echo wp_kses_post('<div class="error"><p><strong>' . sprintf(__('AzamPay requires WooCommerce to be installed and active. Click %s to install WooCommerce.', 'azampay-woo'), '<a href="' . admin_url('plugin-install.php?tab=plugin-information&plugin=woocommerce&TB_iframe=true&width=772&height=539') . '" class="thickbox open-plugin-details-modal">here</a>') . '</strong></p></div>');
+}
+
+/**
+ * Display the test mode notice.
+ * */
+function wc_azampay_testmode_notice() {
+
+    if (!current_user_can('manage_options') || 'woocommerce_page_wc-settings' !== get_current_screen()->id) {
+        return;
+    }
+
+    $azampay_settings = get_option('woocommerce_azampay_settings');
+    $test_mode = isset($azampay_settings['test_mode']) ? $azampay_settings['test_mode'] : '';
+    $enabled = isset($azampay_settings['enabled']) ? $azampay_settings['enabled'] : '';
+    if ('yes' === $enabled && 'yes' === $test_mode) {
+        echo wp_kses_post('<div class="error"><p>' . sprintf(__('AzamPay test mode is still enabled, Click <strong><a href="%s">here</a></strong> to disable it when you want to start accepting live payment on your site.', 'azampay-woo'), esc_url(admin_url('admin.php?page=wc-settings&tab=checkout&section=azampay'))) . '</p></div>');
+    }
+}
